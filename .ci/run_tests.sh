4aceb3d5f4f5f54e110bfe9187cd4b05928a2a00
Paolo Stivanin <info@paolostivanin.com>
2020-04-23 16:47:29 +0200

    Fix script

--- .ci/run_tests.sh
+++ .ci/run_tests.sh
@@ -3,7 +3,9 @@
 set -e
 
 mkdir build && cd $_
-cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DBUILD_TESTING=ON
-make
-./tests/test_all
+cmake .. -DCMAKE_INSTALL_PREFIX=/usr 
+make -j2
 make install
+cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DBUILD_TESTING=ON
+make -j2
+./tests/test_all
